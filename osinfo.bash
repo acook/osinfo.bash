b40756bf6da74fe125ec3830708240c64a31e58f
Anthony Cook <github@anthonymcook.com>
2021-11-21 02:46:45 -0600

    Shellcheck corrections and lib code improvements

--- osinfo.bash
+++ osinfo.bash
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
 
+# used in conditionals to determine presence of a command or executable
+command_exists() { command -v "$1" > /dev/null 2>&1; }
+
 # Gather uname and make it lowercase
-PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
-echo $PLATFORM
+PLATFORM="$(uname | tr '[:upper:]' '[:lower:]')"
+echo "$PLATFORM"
 
 # Detect distro
-DISTRO='unknown'
+DISTRO="unknown"
 case $PLATFORM in
 
   # OS X
   darwin)
-    DISTRO='osx'
+    DISTRO="osx"
   ;;
 
   # Linux
   linux)
     # Gather release info
-    if [[ -n `command -v lsb_release` ]]; then
+    if command_exists lsb_release; then
       # Some distros have an lsb_release command that we can use
       # dump release info and drop version
-      DISTRO=`lsb_release -ds 2>/dev/null | sed 's:^\([[:graph:]]*\) .*:\L\1:'`
-    elif [[ -n `echo /etc/*release` ]]; then
+      DISTRO="$(lsb_release -ds 2>/dev/null | sed 's:^\([[:graph:]]*\) .*:\L\1:')"
+    elif [[ -n $(echo /etc/*release) ]]; then
       # Ohers have a file in /etc with the name of the distro in the filename
       # grab first entry and remove extraneous path and filename affixes
-      DISTRO=`echo /etc/*release | head -1 | cut -d " " -f 1 | sed 's:/etc/\(.*\)-release:\1:'`
+      DISTRO="$(echo /etc/*release | head -1 | cut -d " " -f 1 | sed 's:/etc/\(.*\)-release:\1:')"
     fi
   ;;
 
@@ -33,4 +36,4 @@ DISTRO="$(echo "$DISTRO" | tr '[:upper:]' '[:lower:]')"
 DISTRO="${DISTRO%\"}"
 DISTRO="${DISTRO#\"}"
 
-echo $DISTRO
+echo "$DISTRO"
